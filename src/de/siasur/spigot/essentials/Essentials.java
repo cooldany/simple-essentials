0da0542483f5ccf0faf7d23ec13aef10dcfd6554
siasur <mischa.behrend+git@gmail.com>
2015-09-14 13:36:17 +0200

    Config Part 1
    Creation of blank config.yml and loading of config.yml possible

--- src/de/siasur/spigot/essentials/Essentials.java
+++ src/de/siasur/spigot/essentials/Essentials.java
@@ -1,5 +1,7 @@
 package de.siasur.spigot.essentials;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Essentials extends JavaPlugin {
@@ -12,12 +14,26 @@ public class Essentials extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		createConfig();
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	private void createConfig() {
+		if (!getDataFolder().exists()) {
+			getDataFolder().mkdirs();
+		}
+		
+		File file = new File(getDataFolder(), "config.yml");
+		if (!file.exists()) {
+			getLogger().info("config.yml not found, creating!");
+			saveDefaultConfig();
+		} else {
+			getLogger().info("config.yml found, loading!");
+		}
 	}
 	
 }
