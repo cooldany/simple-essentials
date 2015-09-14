35dbe7b40b7f47e04f06b1106b3a26ca1350830a
Mischa Behrend <mischa.behrend+git@gmail.com>
2015-09-14 13:19:22 +0200

    Added Basics
    - Included Spigot Api
    - Created Main Class

---
+++ src/de/siasur/spigot/essentials/Essentials.java
@@ -0,0 +1,23 @@
+package de.siasur.spigot.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Essentials extends JavaPlugin {
+	
+	public static Essentials instance;
+	
+	public Essentials() {
+		instance = this;
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
