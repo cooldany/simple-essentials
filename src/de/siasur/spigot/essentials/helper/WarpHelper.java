4d453e09818d75682439dbca8518515c45ab7ff4
Mischa Behrend <mischa.behrend+git@gmail.com>
2015-09-30 20:54:20 +0200

    Message Helper

--- src/de/siasur/spigot/essentials/helper/WarpHelper.java
+++ src/de/siasur/spigot/essentials/helper/WarpHelper.java
@@ -1,12 +1,26 @@
 package de.siasur.spigot.essentials.helper;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+
+import de.siasur.spigot.essentials.Essentials;
 
 public class WarpHelper {
 
 	public Location getWarplocation(String warp) {
-		// TODO Auto-generated method stub
-		return null;
+		return getWarplocation(warp, Essentials.instance.getConfig().getBoolean("warp.directional"));
 	}	
+	
+	public Location getWarplocation(String warp, Boolean directional) {
+		ConfigurationSection warpSection = Essentials.instance.warpConfig.getConfigurationSection(warp);
+		String worldName = warpSection.getString("world");
+		World world = Bukkit.getWorld(worldName);
+		
+		// TODO Vernünftig implementieren... :D
+		
+		return null;
+	}
 	
 }
